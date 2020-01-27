ee927ebaa457bbb0a080c4b4193645288511be60
hermannhausherr <hermann.hausherr@gmail.com>
2020-01-27 18:04:02 +0100

    Aggiornamento README

--- prova.c
+++ prova.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	int a = 0;
-	printf("Inserisci il valore di a: ");
-	scanf("%*d %d", &a);
-	printf("Il valore di a e' : %d", a);
+	int a;
+	printf("Il valore e': %d", sizeof(a));
 
 	return 0;
 }
